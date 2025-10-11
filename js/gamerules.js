20f785bb6777e0979576170ec407eb74ff9d32a3
PeterCypers <peter-cypers@hotmail.com>
2025-10-11 16:45:34 +0200

    fixed paths

--- js/gamerules.js
+++ js/gamerules.js
@@ -4,7 +4,7 @@ function navigateTo(relpath) {
 
 function init() {
   const returnBtn = document.getElementById("gamerules_back_arrow");
-  const homePath = "../index.html"; // ./index.html
+  const homePath = "./index.html";
 
   returnBtn.onclick = () => {
     navigateTo(homePath);
